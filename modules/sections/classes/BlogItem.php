1dedb5b71742e35de5df71dfd7dead151d0345dc
Bogdan <bogdanszabo@yahoo.com>
2016-02-17 17:26:29 +0100

    First phase

---
+++ modules/sections/classes/BlogItem.php
@@ -0,0 +1,69 @@
+<?php
+
+class BlogItem extends ObjectModel {
+
+    public $id_blogitem;
+    public $title;
+    public $content;
+    public $timestamp;
+    public $is_big;
+    public $color;
+
+    public static $definition = array(
+        'table' => 'blogitem',
+        'primary' => 'id_blogitem',
+        'multilang' => TRUE,
+        'fields' => array(
+            'color' => array('type' => self::TYPE_STRING, 'required' => TRUE),
+            'title' => array('type' => self::TYPE_STRING, 'lang' => TRUE, 'required' => TRUE),
+            'content' => array('type' => self::TYPE_HTML, 'lang' => TRUE, 'required' => TRUE),
+            'timestamp' => array('type' => self::TYPE_INT),
+            'is_big' => array('type' => self::TYPE_BOOL),
+        ),
+    );
+
+    public function __construct($id = NULL, $lang = NULL, $shop = NULL)
+    {
+        parent::__construct($id, $lang, $shop);
+
+        $this->id = $this->id_blogitem;
+        $this->timestamp = ($this->timestamp === NULL ? time() : $this->timestamp);
+    }
+
+    public function getDate()
+    {
+        return date('j F Y', $this->timestamp);
+    }
+
+    public function getPath()
+    {
+        $path = _PS_IMG_DIR_.'sections'.DIRECTORY_SEPARATOR.'blog-'.$this->id_blogitem.'.jpg';
+
+        if ( ! isset($this->id_blogitem) OR empty($this->id_blogitem) OR ! file_exists($path))
+            return NULL;
+
+        return $path;
+    }
+
+    public function getURL()
+    {
+        if ($this->getPath() === NULL)
+            return NULL;
+
+        return _PS_IMG_.'sections'.DIRECTORY_SEPARATOR.'blog-'.$this->id_blogitem.'.jpg';
+    }
+
+    public static function all($lang = NULL)
+    {
+        if ($lang === NULL)
+        {
+            $lang = Context::getContext()->language->id;
+        }
+
+        $items = new PrestaShopCollection('BlogItem', $lang);
+        $items->orderBy('id_blogitem', 'DESC');
+
+        return $items;
+    }
+
+}
